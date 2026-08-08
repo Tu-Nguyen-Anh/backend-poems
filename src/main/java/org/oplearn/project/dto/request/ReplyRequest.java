63ce7d8bb25e89fe7217c2526fc8793572a2ddfe
Haziii1 <nguyenduchaikm1@gmail.com>
2026-08-08 18:14:30 +0700

    feat(replies): add DTOs and ReplyNotFoundException

---
+++ src/main/java/org/oplearn/project/dto/request/ReplyRequest.java
@@ -0,0 +1,23 @@
+package org.oplearn.project.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReplyRequest {
+  @NotBlank(message = "reply.content.not_blank")
+  @Size(min = 1 , max = 1000, message = "reply.content.max_length")
+  private String content;
+
+  private Long commentId;
+
+  private Long userId;
+}
