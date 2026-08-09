f787bb612b568b2ca520c39df7c31f83bb6a6678
Haziii1 <nguyenduchaikm1@gmail.com>
2026-08-09 12:46:45 +0700

    feat(feedback): add Feedback entity and FeedbackStatus enum

---
+++ src/main/java/org/oplearn/project/entity/Feedback.java
@@ -0,0 +1,33 @@
+package org.oplearn.project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.oplearn.project.entity.base.BaseAuditEntity;
+import org.oplearn.project.entity.base.BaseEntity;
+
+@Entity
+@Table(name = "feedbacks")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Feedback extends BaseAuditEntity {
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Column(name = "poem_id", nullable = false)
+  private Long poemId;
+
+  @Column(columnDefinition = "feedback_status_enum")
+  @Enumerated(EnumType.STRING)
+  @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+  @Builder.Default
+  private FeedbackStatus status = FeedbackStatus.PENDING;
+}
